169fa496437933439738c9ee6addb4dbd23aec0e
Milok Murlyka <tentacles.telari.love@gmail.com>
2024-03-10 22:31:39 +0300

    change dbname

--- mongoose/connection.mjs
+++ mongoose/connection.mjs
@@ -3,4 +3,6 @@ import { MONGO_DB_NAME, MONGO_DB_CONNECTION_URL } from '../config.mjs';
 
 if (!MONGO_DB_NAME || !MONGO_DB_CONNECTION_URL) throw Error('Bad connection to mongo db!');
 
-mongoose.connect(MONGO_DB_CONNECTION_URL);
+mongoose.connect(MONGO_DB_CONNECTION_URL, {
+	dbName: MONGO_DB_NAME,
+});
