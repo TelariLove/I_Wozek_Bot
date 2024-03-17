17766458915de885625b118e8e9d9bc53bece1e1
Milok Murlyka <tentacles.telari.love@gmail.com>
2024-03-17 19:28:54 +0300

    delete command damaged

--- bot/commands.mjs
+++ bot/commands.mjs
@@ -2,7 +2,6 @@ import { bot } from './connecting.mjs';
 
 import { start } from './commands/start.mjs';
 import { end } from './commands/end.mjs';
-import { damaged } from './commands/dagamed.mjs';
 import { pagination } from './commands/pagination.mjs';
 import { help } from './commands/help.mjs';
 import { status } from './commands/status.mjs';
