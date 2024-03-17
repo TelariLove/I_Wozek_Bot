19bd8db241ddad4bd5865904197ada9ecf8639ee
Milok Murlyka <tentacles.telari.love@gmail.com>
2024-03-17 19:29:56 +0300

    delete command page

--- bot/commands.mjs
+++ bot/commands.mjs
@@ -1,7 +1,6 @@
 import { bot } from './connecting.mjs';
 
 import { start } from './commands/start.mjs';
-import { end } from './commands/end.mjs';
 import { pagination } from './commands/pagination.mjs';
 import { help } from './commands/help.mjs';
 import { status } from './commands/status.mjs';
